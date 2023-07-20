2b90a5fa8b52031585af11b07f87db6ef250a710
Davide Mazzonetto <davide.mazzonetto@orangepix.it>
2023-07-20 15:24:02 +0200

    ADD sh tools

---
+++ download-theme.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SERVER_HOST="ecommercepro.orangepix.it"
+SERVER_USER="wasabi"
+SERVER_PATH="/var/www/vhosts/wasabi.i-dealsrl.com/httpdocs/"
+
+THEME_DIR="themes/orange-theme-8/"
+
+rsync -avzP --exclude 'node_modules' $SERVER_USER@$SERVER_HOST:$SERVER_PATH$THEME_DIR $THEME_DIR
