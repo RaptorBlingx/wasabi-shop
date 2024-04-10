8b3226e49b595f3396bbd6a60553d9fc0167bafe
Andrea Pozza <andreapozza@orangepix.it>
2024-04-10 18:17:57 +0200

    Change Customer override

--- override/classes/Customer.php
+++ override/classes/Customer.php
@@ -3,34 +3,45 @@ class Customer extends CustomerCore
 {
     /*
     * module: developers
-    * date: 2024-03-28 14:23:57
+    * date: 2024-04-10 17:59:26
     * version: 0.0.1
     */
     public $developer_status;
     /*
     * module: developers
-    * date: 2024-03-28 14:23:57
+    * date: 2024-04-10 17:59:26
     * version: 0.0.1
     */
     public function __construct($id = null)
     {
-        self::$definition['fields']['developer_status'] = [
-            'type' => self::TYPE_STRING
-        ];
+        self::setupDefinition();
         parent::__construct($id);
     }
     /*
     * module: developers
-    * date: 2024-03-28 14:23:57
+    * date: 2024-04-10 17:59:26
     * version: 0.0.1
     */
     public static function getDevelopers($active = true)
     {
-        $collection = (new PrestaShopCollection('Customer'))
-            ->sqlWhere('`deveveloper_status` IS NOT NULL');
+        self::setupDefinition();
+        $collection = (new PrestaShopCollection(self::class))
+            ->sqlWhere('`developer_status` IS NOT NULL')
+            ->where('developer_status', '!=', '');
         if ($active) {
             $collection->where('developer_status', '=', Developers::STATUS_APPROVED);
         }
         return $collection->getResults();
+    }
+    /*
+    * module: developers
+    * date: 2024-04-10 17:59:26
+    * version: 0.0.1
+    */
+    protected static function setupDefinition()
+    {
+        self::$definition['fields']['developer_status'] = [
+            'type' => self::TYPE_STRING
+        ];
     }
 }
