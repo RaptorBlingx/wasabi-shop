0b641f50b67aa29a0c9d1a8ed7c20e4f329e1e14
Andrea Pozza <andrea.pozza@orangepix.it>
2024-12-17 09:19:24 +0100

    Change AddProductFromDataspace
    - Assign existing products to seller
    - Assign tax rules group
    - Add image product

--- AddProductFromDataspace.php
+++ AddProductFromDataspace.php
@@ -28,13 +28,19 @@ $sellerID = $data->Candidate_Seller_ID;
 unset($data->Candidate_Seller_ID);
 $productId = Product::getIdByReference($data->SkillID);
 
+// Search seller by WLS_ID
+$seller = (new PrestaShopCollection('Customer'))->where('wls_id', '=', $sellerID)->getFirst();
+
 if ($productId) {
     // Product already exists
+    if ($seller instanceof Customer) {
+        Ets_mp_product::addProductSeller($productId, $seller->id);
+    }
     header('Content-Type: application/json');
-    http_response_code(409);
+    http_response_code(200);
     echo json_encode(
         [
-            'error' => 'Product already exists',
+            'success' => $productId
         ]
     );
     exit;
@@ -59,6 +65,7 @@ $product->description_short = array_fill_keys(
 );
 $product->product_type = ProductType::TYPE_VIRTUAL;
 $product->is_virtual = true;
+$product->id_tax_rules_group = TaxRulesGroup::getIdByName('EU VAT For Virtual Products');
 
 //this needs to be a pre + maybe if we had the existing one we could limit the list in the ui?
 
@@ -71,8 +78,6 @@ if ($product->add()) {
     $product->addToCategories(array(12)); // Replace with your category IDs
     StockAvailable::setQuantity($product->id, 0, 100); // 100 is the stock quantity
 
-    // Search seller by WLS_ID
-    $seller = (new PrestaShopCollection('Customer'))->where('wls_id', '=', $sellerID)->getFirst();
     if ($seller instanceof Customer) {
         Ets_mp_product::addProductSeller($product->id, $seller->id);
     }
@@ -83,7 +88,21 @@ if ($product->add()) {
         $amazzingfilter->indexProduct([$product->id]);
     }
 
+    // Image generation
+    $image = new Image();
+    $image->id_product = $product->id;
+    $image->cover = true;
+    $source_file = __DIR__ . '/img/p/skill.jpg'; // can be url or local path
+    if ($image->add()) {
+        $shops = Shop::getShops(true, null, true);
+        $image->associateTo($shops);
+        $copyImg = new ReflectionMethod('AdminImportControllerCore', 'copyImg');
+        $copyImg->setAccessible(true);
+        $copyImg->invoke(null, $product->id, $image->id, $source_file, 'products', true);
+    }
+
     header('Content-Type: application/json');
+    http_response_code(201);
     echo    json_encode([   'success' => $product->id]) ;
 } else {
     header('Content-Type: application/json');
